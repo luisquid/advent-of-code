93ed795bd81d0d0c1dc8858f2fd5b7ee1db4a2d2
squid <luisquid.dev@gmail.com>
2022-12-05 00:24:14 -0500

    Start on day 05

---
+++ 2022/Day05/Day05.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2022
+{
+    class Day05 {         
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"./day05-input.txt");
+            string [] data = text.Split('\n');
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                string [] words = data[i].Split(' ');
+                Console.WriteLine(words[1]);
+            }
+        }
+    }
+}
